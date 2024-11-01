c8919051d66118652a4e8bd51a28d35af7f02dde
Yuri <142381932+Yurei04@users.noreply.github.com>
2024-11-01 13:57:42 +0800

    Adding Chart type

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const form = document.getElementById("countryForm");
 const ctx = document.getElementById("inflationChart").getContext("2d");
+const chartTypeInput = document.getElementById("chartType");
 
 let inflationChart;
 
 async function obtainData(code) {
-    const apiURL = ``;
+    const apiURL = `${code}`;
 
     try {
         const response = await fetch(apiURL);
@@ -26,5 +27,15 @@ async function obtainData(code) {
         return [];
     }
 }
+
+form.addEventListener("submit", async (e) => {
+    e.defaultPrevented();
+
+    const countryCode = document.getElementById("country").value;
+    const chartType = chartTypeInput.value;
+
+    const inflationData = await obtainData(countryCode);
+
+})
 
 
